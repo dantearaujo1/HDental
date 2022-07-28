c42ca9741c37d3f8807f082ef66da98503681197
dantearaujo1 <danteeng@hotmail.com>
2022-07-27 22:32:33 -0300

    Adding ProcedureScreen to the StackNavigator

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import ClinicScreen from './src/screens/ClinicScreen';
 import AllClinicsScreen from './src/screens/AllClinicsScreen';
 import ClinicDetailScreen from './src/screens/ClinicDetailScreen';
 import AddClinicScreen from './src/screens/AddClinicScreen';
+import ProcedureScreen from './src/screens/ProcedureScreen';
 
 
 const Stack = createStackNavigator();
@@ -17,10 +18,11 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown:false}}/>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="Clinics" component={ClinicScreen} options={{headerShown:false,headerStyle:{backgroundColor:"#898C8B"}}}/>
+        <Stack.Screen name="Clinics" component={ClinicScreen} options={{headerShown:true,headerStyle:{backgroundColor:"#898C8B"}}}/>
         <Stack.Screen name="AllClinics" component={AllClinicsScreen} options={{headerShown:true,headerStyle:{backgroundColor:"#898C8B"}}}/>
-        <Stack.Screen name="ClinicDetail" component={ClinicDetailScreen} options={{headerShown:false,headerStyle:{backgroundColor:"#898C8B"}}}/>
+        <Stack.Screen name="ClinicDetail" component={ClinicDetailScreen} options={{headerShown:true,headerStyle:{backgroundColor:"#898C8B"}}}/>
         <Stack.Screen name="AddClinic" component={AddClinicScreen} options={{headerShown:true,headerStyle:{backgroundColor:"#898C8B"}}}/>
+        <Stack.Screen name="Procedures" component={ProcedureScreen} options={{headerShown:true,headerStyle:{backgroundColor:"#898C8B"}}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
